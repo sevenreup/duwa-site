2177e7debbfa189b213fb9c86d706484fe49aee5
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-10-31 20:52:21 +0200

    Update intro.ts

--- src/pages/video/intro.ts
+++ src/pages/video/intro.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params, request, redirect }) => {
-  return redirect("https://www.youtube.com");
+  return redirect("https://www.youtube.com", 301);
 };
