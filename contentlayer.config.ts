a3d1d001c8b6c4df8d2a978013ef6c66a31a6598
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-11-21 13:54:37 +0200

    update

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -85,6 +85,7 @@ export default makeSource(async () => {
   const prettyCodeOpts: PrettyOptions = {
     theme: "one-dark-pro",
     defaultLang: "duwa",
+    bypassInlineCode: true,
     onVisitLine(node) {
       // Prevent lines from collapsing in `display: grid` mode, and allow empty
       // lines to be copy/pasted
